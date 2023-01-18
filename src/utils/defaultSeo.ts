d00b33e4a70fc0dcca3196f2824ed854c58c7d90
Supakarn Laorattanakul <wearedprompt@gmail.com>
2023-01-18 10:10:42 +0700

    change to default title

--- src/utils/defaultSeo.ts
+++ src/utils/defaultSeo.ts
@@ -1,7 +1,7 @@
 import { DefaultSeoProps } from 'next-seo'
 
 const config: DefaultSeoProps = {
-  title: 'Apple TH Inventory Checker',
+  defaultTitle: 'Apple TH Inventory Checker',
   description: 'Apple TH Inventory Checker',
   titleTemplate: '%s | Apple TH Inventory Checker',
 }
